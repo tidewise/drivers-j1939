118b40d56578faeaf790b60d22b19027b75237e5
seixasxbr <mateus.seixas@tidewise.io>
2026-02-10 14:29:59 -0300

    feat: add helpers

---
+++ src/Helpers.cpp
@@ -0,0 +1,20 @@
+#include <j1939/Helpers.hpp>
+
+using namespace nmea2000;
+
+namespace j1939 {
+    bool isBAM(Message const& message)
+    {
+        return message.payload[0] == TP_CM_BAM;
+    }
+
+    bool isConnectionManagement(Message const& message)
+    {
+        return message.pgn == PGN_TP_CM;
+    }
+
+    bool isDataTransfer(Message const& message)
+    {
+        return message.pgn == PGN_TP_DT;
+    }
+}
