1a923e1add0d8cf2c0078c8526e2d0666cf1c075
seixasxbr <mateus.seixas@tidewise.io>
2026-02-11 11:02:18 -0300

    fix: improve isBAM check

--- src/Helpers.cpp
+++ src/Helpers.cpp
@@ -5,7 +5,7 @@ using namespace nmea2000;
 namespace j1939 {
     bool isBAM(Message const& message)
     {
-        return message.payload[0] == TP_CM_BAM;
+        return message.pgn == PGN_TP_CM && message.payload[0] == TP_CM_BAM;
     }
 
     bool isConnectionManagement(Message const& message)
